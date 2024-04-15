e2a4f12f0eab2041fd0114bc7cecd11f688fdee4
watess01 <stephen.wates@uicentric.com>
2024-04-15 16:30:00 +0100

    Bean list

--- mydvd/src/main/java/com/mylib/myDvd/MyDvdApplication.java
+++ mydvd/src/main/java/com/mylib/myDvd/MyDvdApplication.java
@@ -1,14 +1,35 @@
 package com.mylib.myDvd;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class
 MyDvdApplication {
 
 	public static void main(String[] args) {
+		System.out.println("start SpringBoot...");
 		SpringApplication.run(MyDvdApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		System.out.println("commandLineRunner...");
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 }
